74cadee269c13d04ad73b3dbe4c520ca72aeac1c
Andrea Diegaardt <atdiegaardt@gmail.com>
2023-04-13 15:56:55 +0200

    add files needed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
+/* eslint-disable import/no-cycle */
+
 import React from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-import Greeting from './components/Greeting';
-import Home from './components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  createAsyncThunk,
+  createSlice,
+  configureStore,
+} from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import Greeting from './greeting';
+
+// Fetch a random message from the API endpoint
+async function getRandomMessageAPI() {
+  return fetch('http://127.0.0.1:3000/api/greetings')
+    .then((response) => response.json())
+    .then((messageData) => messageData);
+}
+
+// Create a thunk function based on the API response
+export const getRandomMessageThunk = createAsyncThunk(
+  'Messages/random',
+  getRandomMessageAPI,
+);
+
+// Create the reducer/actions
+const handleMessages = createSlice({
+  name: 'reduce',
+  initialState: { value: null },
+  extraReducers: (builder) => {
+    builder.addCase(getRandomMessageThunk.fulfilled, (state, action) => {
+      state.value = action.payload.text;
+    });
+  },
+});
+
+const store = configureStore({
+  reducer: handleMessages.reducer,
+});
 
 function App() {
   return (
-    <div>
-      <Link to="/greeting">View Greetings</Link>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/greeting" element={<Greeting />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
