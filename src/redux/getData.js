c7785997be48c11e24f0ff180e92507e028f40aa
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-04-14 12:22:05 +0200

    Update getData.js

--- src/redux/getData.js
+++ src/redux/getData.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const getData = createAsyncThunk('greetings', async () => {
   try {
-    const text = await fetch('http://127.0.0.1:5000/api/greetings');
+    const text = await fetch('http://127.0.0.1:3000/api/greetings');
     const data = await text.json();
 
     if (data.greeting) {
